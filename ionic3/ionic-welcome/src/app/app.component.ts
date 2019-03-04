f0ccfebaeeb5b9fbd0be45e86a438b5a9489942d
Ashish <reytum@live.com>
2019-03-04 14:08:54 +0530

    add broadcaster plugin demo for start new conversation

--- ionic3/ionic-welcome/src/app/app.component.ts
+++ ionic3/ionic-welcome/src/app/app.component.ts
@@ -15,8 +15,43 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      // We are registering the native event listener here 
+      // so that we can check for either startNewConversation or logout event from the native screen
+    if(platform.is('android')){
+      //this is a brodcaster plugin. Add it using command 'ionic cordova plugin add cordova-plugin-broadcaster'
+      //this KmStartNewConversation event is fired when the start new conversation button is clicked on the chat list screen
+      broadcaster.addEventListener("KmStartNewConversation", (e) => {
+          //once the event is received we can do whatever we want.
+          this.createNewChat();
+       });
+      }
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+
+  createNewChat(){
+    let convInfo = {
+     'agentIds':['reytum@live.com'],  //Array of agentIds
+     'botIds': ['liz']  //Array of botIds
+    };
+    
+ //Creating a new conversation
+ kommunicate.startNewConversation(convInfo, (response) => {
+     let convObj = {
+       'clientChannelKey' : response,
+       'takeOrder' : true
+     };
+     //launching the conversation as soon as it gets created
+    kommunicate.launchParticularConversation(convObj, function(response) {
+     }, function(response) {
+     });
+     console.log("Kommunicate create conversation success: " + response);
+   },(response) => {
+     console.log("Kommunicate create conversation failed : " + response);
+   });
+  }
 }
+
+declare var kommunicate : any;
+declare var broadcaster : any;
